87a8c044d0f7efbcc564e862227bf168609adfd0
adwalvekar <aditya.s.walvekar@gmail.com>
2022-09-25 11:46:14 -0400

    Initial commit with the latest files from

---
+++ service/routes.py
@@ -0,0 +1,35 @@
+"""
+My Service
+
+Describe what your service does here
+"""
+
+from flask import Flask, jsonify, request, url_for, make_response, abort
+from .common import status  # HTTP Status Codes
+from service.models import YourResourceModel
+
+# Import Flask application
+from . import app
+
+
+######################################################################
+# GET INDEX
+######################################################################
+@app.route("/")
+def index():
+    """ Root URL response """
+    return (
+        "Reminder: return some useful information in json format about the service here",
+        status.HTTP_200_OK,
+    )
+
+
+######################################################################
+#  U T I L I T Y   F U N C T I O N S
+######################################################################
+
+
+def init_db():
+    """ Initializes the SQLAlchemy app """
+    global app
+    YourResourceModel.init_db(app)
