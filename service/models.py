2c45e05fd002fcd08edee592df802ee0fff4d8a5
Aman Chopra <amanchopra64@gmail.com>
2022-10-08 15:21:50 -0400

    Created the schema of Promotion

--- service/models.py
+++ service/models.py
@@ -1,9 +1,28 @@
 """
-Models for YourResourceModel
+Models for Promotion
 
 All of the models are stored in this module
+
+Models
+------
+Promotion - A Promotion used in the Shopping Cart
+Attributes:
+-----------
+name (string) - the name of the promotion
+type (enumeration) - the type of the promotion like ABS_DISCOUNT, PERCENT_DISCOUNT
+description (string) - the description of the promotion
+promotion_value (number) - the value of the promotion like 2000 off,
+promotion_percent: the percent of the promotion like 50% off,
+status (boolean) - True for active promotions,
+expiry (date) - Date when the promotion expires,
+created_at (date) - Date when the promotion was created,
+last_updated_at (date) - Date when the promotion was last updated
+
 """
 import logging
+from enum import Enum
+from datetime import date
+from datetime import timedelta
 from flask_sqlalchemy import SQLAlchemy
 
 logger = logging.getLogger("flask.app")
@@ -11,72 +30,129 @@ logger = logging.getLogger("flask.app")
 # Create the SQLAlchemy object to be initialized later in init_db()
 db = SQLAlchemy()
 
+def init_db(app):
+    """Initialize the SQLAlchemy app"""
+    Promotion.init_db(app)
 
 class DataValidationError(Exception):
-    """ Used for an data validation errors when deserializing """
+    """Used for an data validation errors when deserializing """
 
-    pass
+class PromotionType(Enum):
+    """Enumeration of valid Promotion Types"""
+
+    ABS_DISCOUNT = 0
+    PERCENT_DISCOUNT = 1
+    DELIVERY_DISCOUNT = 3
+    GIFT_CARDS = 4
 
 
-class YourResourceModel(db.Model):
+class Promotion(db.Model):
     """
-    Class that represents a YourResourceModel
+    Class that represents a Promotion
+
+    This version uses a relational database for persistence which is hidden
+    from us by SQLAlchemy's object relational mappings (ORM)
     """
 
     app = None
 
     # Table Schema
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(63))
+    name = db.Column(db.String(63), nullable=False)
+    type = db.Column(
+        db.Enum(PromotionType), nullable=False, server_default=(PromotionType.ABS_DISCOUNT.name)
+    )
+    description = db.Column(db.String(63), nullable=False)
+    promotion_value = db.Column(db.Integer)
+    promotion_percent = db.Column(db.Integer)
+    status = db.Column(db.Boolean(), nullable=False, default=True)
+    expiry = db.Column(db.Date(), nullable=False, default = date.today() + timedelta(days=7))
+    created_at = db.Column(db.Date(), nullable=False, default=date.today())
+    last_updated_at = db.Column(db.Date(), nullable=False, default=date.today())
+
+    # Instance Methods
 
     def __repr__(self):
-        return "<YourResourceModel %r id=[%s]>" % (self.name, self.id)
+        return f"<Promotion {self.name} id=[{self.id}]>"
 
     def create(self):
         """
-        Creates a YourResourceModel to the database
+        Creates a Promotion to the database
         """
         logger.info("Creating %s", self.name)
-        self.id = None  # id must be none to generate next primary key
+        self.id = None  # pylint: disable=invalid-name
         db.session.add(self)
         db.session.commit()
 
     def update(self):
         """
-        Updates a YourResourceModel to the database
+        Updates a Promotion to the database
         """
         logger.info("Saving %s", self.name)
+        if not self.id:
+            raise DataValidationError("Update called with empty ID field")
         db.session.commit()
 
     def delete(self):
-        """ Removes a YourResourceModel from the data store """
+        """ Removes a Promotion from the data store """
         logger.info("Deleting %s", self.name)
         db.session.delete(self)
         db.session.commit()
 
-    def serialize(self):
-        """ Serializes a YourResourceModel into a dictionary """
-        return {"id": self.id, "name": self.name}
+    def serialize(self) -> dict:
+        """ Serializes a Promotion into a dictionary """
+        return {
+            "id": self.id,
+            "name": self.name,
+            "type": self.type.name, # convert enum to string
+            "description": self.description,
+            "promotion_value": self.promotion_value,
+            "promotion_percent": self.promotion_percent,
+            "status": self.status,
+            "expiry": self.expiry.isoformat(),
+            "created_at": self.created_at.isoformat(),
+            "last_updated_at": self.last_updated_at.isoformat()
+        }
 
     def deserialize(self, data):
         """
-        Deserializes a YourResourceModel from a dictionary
+        Deserializes a Promotion from a dictionary
 
         Args:
-            data (dict): A dictionary containing the resource data
+            data (dict): A dictionary containing the promotion data
         """
         try:
             self.name = data["name"]
+            self.type = getattr(PromotionType, data["type"])  # create enum from string
+            self.description = data["description"]
+            self.promotion_value = data["promotion_value"]
+            self.promotion_percent = data["promotion_percent"]
+            if isinstance(data["status"], bool):
+                self.status = data["status"]
+            else:
+                raise DataValidationError(
+                    "Invalid type for boolean [status]: "
+                    + str(type(data["status"]))
+                )
+            self.expiry = date.fromisoformat(data["expiry"])
+            self.created_at = date.fromisoformat(data["created_at"])
+            self.last_updated_at = date.fromisoformat(data["last_updated_at"])
+        except AttributeError as error:
+            raise DataValidationError(
+                "Invalid attribute: " + error.args[0]
+            ) from error
         except KeyError as error:
             raise DataValidationError(
-                "Invalid YourResourceModel: missing " + error.args[0]
-            )
+                "Invalid Promotion: missing " + error.args[0]
+            ) from error
         except TypeError as error:
             raise DataValidationError(
-                "Invalid YourResourceModel: body of request contained bad or no data - "
-                "Error message: " + error
-            )
+                "Invalid Promotion: body of request contained bad or no data - "
+                "Error message: " + str(error)
+            ) from error
         return self
+
+    # Class Methods
 
     @classmethod
     def init_db(cls, app):
@@ -90,22 +166,22 @@ class YourResourceModel(db.Model):
 
     @classmethod
     def all(cls):
-        """ Returns all of the YourResourceModels in the database """
-        logger.info("Processing all YourResourceModels")
+        """ Returns all of the Promotions in the database """
+        logger.info("Processing all Promotions")
         return cls.query.all()
 
     @classmethod
     def find(cls, by_id):
-        """ Finds a YourResourceModel by it's ID """
+        """ Finds a Promotion by it's ID """
         logger.info("Processing lookup for id %s ...", by_id)
         return cls.query.get(by_id)
 
     @classmethod
     def find_by_name(cls, name):
-        """Returns all YourResourceModels with the given name
+        """Returns all Promotion with the given name
 
         Args:
-            name (string): the name of the YourResourceModels you want to match
+            name (string): the name of the Promotion we want to match
         """
         logger.info("Processing name query for %s ...", name)
         return cls.query.filter(cls.name == name)
--- tests/test_models.py
+++ tests/test_models.py
@@ -1,40 +1,261 @@
 """
-Test cases for YourResourceModel Model
+Test cases for Promotion Model
 
+Test cases for Promotion Model
+Test cases can be run with:
+    nosetests
+    coverage report -m
 """
 import os
 import logging
 import unittest
-from service.models import YourResourceModel, DataValidationError, db
+from datetime import date
+from service.models import Promotion, PromotionType, DataValidationError, db
+from service import app
+from tests.factories import PromotionFactory
+
+DATABASE_URI = os.getenv(
+    "DATABASE_URI", "postgresql://postgres:postgres@localhost:5432/testdb"
+)
 
 ######################################################################
-#  <your resource name>   M O D E L   T E S T   C A S E S
+#  P R O M O T I O N   M O D E L   T E S T   C A S E S
 ######################################################################
-class TestYourResourceModel(unittest.TestCase):
-    """ Test Cases for YourResourceModel Model """
+
+
+class TestPromotion(unittest.TestCase):
+    """ Test Cases for Promotion Model """
 
     @classmethod
     def setUpClass(cls):
         """ This runs once before the entire test suite """
-        pass
+        app.config["TESTING"] = True
+        app.config["DEBUG"] = False
+        app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+        app.logger.setLevel(logging.CRITICAL)
+        Promotion.init_db(app)
 
     @classmethod
     def tearDownClass(cls):
         """ This runs once after the entire test suite """
-        pass
+        db.session.close()
 
     def setUp(self):
         """ This runs before each test """
-        pass
+        db.session.query(Promotion).delete()  # clean up the last tests
+        db.session.commit()
 
     def tearDown(self):
         """ This runs after each test """
-        pass
+        db.session.remove()
 
     ######################################################################
     #  T E S T   C A S E S
     ######################################################################
 
-    def test_XXXX(self):
-        """ It should always be true """
-        self.assertTrue(True)
+    def test_create_a_promotion(self):
+        """It should Create a promotion and assert that it exists"""
+        promotion = Promotion(name="Sale", type=PromotionType.ABS_DISCOUNT,
+                              description="Sale on Products", promotion_value=1000,
+                              promotion_percent=0)
+        self.assertEqual(str(promotion), "<Promotion Sale id=[None]>")
+        self.assertTrue(promotion is not None)
+        self.assertEqual(promotion.id, None)
+        self.assertEqual(promotion.name, "Sale")
+        self.assertEqual(promotion.type, PromotionType.ABS_DISCOUNT)
+        self.assertEqual(promotion.description, "Sale on Products")
+        self.assertEqual(promotion.promotion_value, 1000)
+        self.assertEqual(promotion.promotion_percent, 0)
+        promotion = Promotion(name="Sale", type=PromotionType.PERCENT_DISCOUNT,
+                              description="Sale on Products", promotion_value=0,
+                              promotion_percent=50, status=False)
+        self.assertEqual(promotion.type, PromotionType.PERCENT_DISCOUNT)
+        self.assertEqual(promotion.promotion_percent, 50)
+        self.assertEqual(promotion.status, False)
+
+    def test_add_a_promotion(self):
+        """It should Create a promotion and add it to the database"""
+        promotions = Promotion.all()
+        self.assertEqual(promotions, [])
+        promotion = Promotion(name="Sale", type=PromotionType.ABS_DISCOUNT,
+                              description="Sale on Products", promotion_value=1000,
+                              promotion_percent=0, status=True)
+        self.assertTrue(promotion is not None)
+        self.assertEqual(promotion.id, None)
+        promotion.create()
+        # Assert that it was assigned an id and shows up in the database
+        self.assertIsNotNone(promotion.id)
+        promotions = Promotion.all()
+        self.assertEqual(len(promotions), 1)
+
+    def test_read_a_promotion(self):
+        """It should Read a Promotion"""
+        promotion = PromotionFactory()
+        logging.debug(promotion)
+        promotion.id = None
+        promotion.create()
+        self.assertIsNotNone(promotion.id)
+        # Fetch it back
+        found_promotion = Promotion.find(promotion.id)
+        self.assertEqual(found_promotion.id, found_promotion.id)
+        self.assertEqual(found_promotion.name, found_promotion.name)
+        self.assertEqual(found_promotion.description,
+                         found_promotion.description)
+
+    def test_update_a_promotion(self):
+        """It should Update a Promotion"""
+        promotion = PromotionFactory()
+        logging.debug(promotion)
+        promotion.id = None
+        promotion.create()
+        logging.debug(promotion)
+        self.assertIsNotNone(promotion.id)
+        # Change it an save it
+        promotion.description = "A new promotion"
+        original_id = promotion.id
+        promotion.update()
+        self.assertEqual(promotion.id, original_id)
+        self.assertEqual(promotion.description, "A new promotion")
+        # Fetch it back and make sure the id hasn't changed
+        # But the data did change
+        promotions = promotion.all()
+        self.assertEqual(len(promotions), 1)
+        self.assertEqual(promotions[0].id, original_id)
+        self.assertEqual(promotions[0].description, "A new promotion")
+
+    def test_update_no_id(self):
+        """It should not Update a Promotion with no id"""
+        promotion = PromotionFactory()
+        logging.debug(promotion)
+        promotion.id = None
+        self.assertRaises(DataValidationError, promotion.update)
+
+    def test_delete_a_promotion(self):
+        """It should Delete a Promotion"""
+        promotion = PromotionFactory()
+        promotion.create()
+        self.assertEqual(len(Promotion.all()), 1)
+        # Delete the promotion and make sure it isn't in the database
+        promotion.delete()
+        self.assertEqual(len(Promotion.all()), 0)
+
+    def test_list_all_promotions(self):
+        """It should List all Promotions in the database"""
+        promotions = Promotion.all()
+        self.assertEqual(promotions, [])
+        # Create 5 Promotions
+        for _ in range(5):
+            promotion = PromotionFactory()
+            promotion.create()
+        # See if we get back 5 promotions
+        promotions = Promotion.all()
+        self.assertEqual(len(promotions), 5)
+
+    def test_serialize_a_promotion(self):
+        """It should serialize a Promotion"""
+        promotion = PromotionFactory()
+        data = promotion.serialize()
+        self.assertNotEqual(data, None)
+        self.assertIn("id", data)
+        self.assertEqual(data["id"], promotion.id)
+        self.assertIn("name", data)
+        self.assertEqual(data["name"], promotion.name)
+        self.assertIn("type", data)
+        self.assertEqual(data["type"], promotion.type.name)
+        self.assertIn("description", data)
+        self.assertEqual(data["description"], promotion.description)
+        self.assertIn("promotion_value", data)
+        self.assertEqual(data["promotion_value"], promotion.promotion_value)
+        self.assertIn("promotion_percent", data)
+        self.assertEqual(data["promotion_percent"],
+                         promotion.promotion_percent)
+        self.assertIn("status", data)
+        self.assertEqual(data["status"], promotion.status)
+        self.assertIn("expiry", data)
+        self.assertEqual(date.fromisoformat(data["expiry"]), promotion.expiry)
+        self.assertIn("created_at", data)
+        self.assertEqual(date.fromisoformat(
+            data["created_at"]), promotion.created_at)
+        self.assertIn("last_updated_at", data)
+        self.assertEqual(date.fromisoformat(
+            data["last_updated_at"]), promotion.last_updated_at)
+
+    def test_deserialize_a_promotion(self):
+        """It should de-serialize a Promotion"""
+        data = PromotionFactory().serialize()
+        promotion = Promotion()
+        promotion.deserialize(data)
+        self.assertNotEqual(promotion, None)
+        self.assertEqual(promotion.id, None)
+        self.assertEqual(promotion.name, data["name"])
+        self.assertEqual(promotion.type.name, data["type"])
+        self.assertEqual(promotion.description, data["description"])
+        self.assertEqual(promotion.promotion_value, data["promotion_value"])
+        self.assertEqual(promotion.promotion_percent,
+                         data["promotion_percent"])
+        self.assertEqual(promotion.status, data["status"])
+        self.assertEqual(promotion.expiry, date.fromisoformat(data["expiry"]))
+        self.assertEqual(promotion.created_at,
+                         date.fromisoformat(data["created_at"]))
+        self.assertEqual(promotion.last_updated_at,
+                         date.fromisoformat(data["last_updated_at"]))
+
+    def test_deserialize_missing_data(self):
+        """It should not deserialize a Promotion with missing data"""
+        data = {"id": 1, "name": "Promotion",
+                "description": "A great Promotion"}
+        promotion = Promotion()
+        self.assertRaises(DataValidationError, promotion.deserialize, data)
+
+    def test_deserialize_bad_data(self):
+        """It should not deserialize bad data"""
+        data = "this is not a dictionary"
+        promotion = Promotion()
+        self.assertRaises(DataValidationError, promotion.deserialize, data)
+
+    def test_deserialize_bad_available(self):
+        """It should not deserialize a bad available attribute"""
+        test_promotion = PromotionFactory()
+        data = test_promotion.serialize()
+        data["status"] = "true"
+        promotion = Promotion()
+        self.assertRaises(DataValidationError, promotion.deserialize, data)
+
+    def test_deserialize_bad_type(self):
+        """It should not deserialize a bad type attribute"""
+        test_promotion = PromotionFactory()
+        data = test_promotion.serialize()
+        data["type"] = "random"
+        promotion = Promotion()
+        self.assertRaises(DataValidationError, promotion.deserialize, data)
+
+    def test_find_promotion(self):
+        """It should Find a Pet by ID"""
+        promotions = PromotionFactory.create_batch(5)
+        for promotion in promotions:
+            promotion.create()
+        logging.debug(promotions)
+        # Make sure they got saved
+        self.assertEqual(len(Promotion.all()), 5)
+        # Find the 2nd pet in the list
+        promotion = Promotion.find(promotions[1].id)
+        self.assertIsNot(promotion, None)
+        self.assertEqual(promotion.id, promotions[1].id)
+        self.assertEqual(promotion.name, promotions[1].name)
+        self.assertEqual(promotion.type, promotions[1].type)
+        self.assertEqual(promotion.status, promotions[1].status)
+        self.assertEqual(promotion.expiry, promotions[1].expiry)
+
+    def test_find_by_name(self):
+        """It should Find a Promotion by Name"""
+        promotions = PromotionFactory.create_batch(5)
+        for promotion in promotions:
+            promotion.create()
+        name = promotions[0].name
+        found = Promotion.find_by_name(name)
+        self.assertEqual(found.count(), 1)
+        self.assertEqual(found[0].name, promotions[0].name)
+        self.assertEqual(found[0].type, promotions[0].type)
+        self.assertEqual(found[0].description, promotions[0].description)
+        self.assertEqual(found[0].status, promotions[0].status)
+        self.assertEqual(found[0].expiry, promotions[0].expiry)
