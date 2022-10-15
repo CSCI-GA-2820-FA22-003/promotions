876d58847d434916bf9b7d6b330d02e08598c411
Awesalot <adidev.vinaik@gmail.com>
2022-10-15 02:30:47 +0000

    Fixed linting issues based on flake8 output Updated routes.py file to add ability to list all promotions Updated test_routes.py file to test the above functionality

--- service/common/error_handlers.py
+++ service/common/error_handlers.py
@@ -39,7 +39,9 @@ def bad_request(error):
     app.logger.warning(message)
     return (
         jsonify(
-            status=status.HTTP_400_BAD_REQUEST, error="Bad Request", message=message
+            status=status.HTTP_400_BAD_REQUEST,
+            error="Bad Request",
+            message=message
         ),
         status.HTTP_400_BAD_REQUEST,
     )
@@ -51,7 +53,11 @@ def not_found(error):
     message = str(error)
     app.logger.warning(message)
     return (
-        jsonify(status=status.HTTP_404_NOT_FOUND, error="Not Found", message=message),
+        jsonify(
+            status=status.HTTP_404_NOT_FOUND,
+            error="Not Found",
+            message=message
+        ),
         status.HTTP_404_NOT_FOUND,
     )
 
